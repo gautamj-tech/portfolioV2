9306181800ade98fc318de337d2e9ba8e3088094
bawantha <bawantha@orelit.com>
2023-06-03 17:23:10 +0530

    guestbook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, session }: any) {
   return (
     <>
       <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <SessionProvider session={session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
       </ThemeProvider>
     </>
   );
--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "../config/keys";
 import { AiOutlineGithub } from "react-icons/ai";
 import ContentLoader from "react-content-loader";
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@/config/keys";
 
-const GuestBook = () => {
-  const { data: session } = useSession<any>();
-  const [loading, setLoading] = useState<any>(false);
+const Guestbook = () => {
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState<boolean>(false);
   const [message, setMessage] = useState<any>("");
   const [userMsg, setUserMsg] = useState<any>([]);
 
@@ -54,7 +54,99 @@ const GuestBook = () => {
     }
   };
 
-  return <div>GuestBook</div>;
+  return (
+    <div>
+      <div>
+        <span>GuestBook</span>
+      </div>
+
+      <div>
+        {session ? (
+          <>
+            <div>
+              <h4>sign in as </h4>
+              <div>
+                <form onSubmit={createMessage}>
+                  <input
+                    type="text"
+                    value={userMsg}
+                    onChange={handleInput}
+                    placeholder="Your Message "
+                  />
+                  <button type="submit">Submit</button>
+                </form>
+              </div>
+
+              <button onClick={() => signOut()}>
+                <span>
+                  <AiOutlineGithub size={20} />
+                </span>{" "}
+                Sign out
+              </button>
+            </div>
+            <div>
+              {loading && (
+                <ContentLoader
+                  speed={2}
+                  width="100%"
+                  height={160}
+                  viewBox="0 0 1024 160"
+                  backgroundColor="#f3f3f3"
+                  foregroundColor="#ecebeb"
+                >
+                  <rect x="-1" y="48" rx="3" ry="3" width="100%" height="40" />
+                  <rect x="-1" y="48" rx="3" ry="3" width="100%" height="40" />
+                  <rect x="-1" y="48" rx="3" ry="3" width="100%" height="40" />
+                </ContentLoader>
+              )}
+              {message &&
+                message.map((item: any, index: number) => (
+                  <div key={index}>
+                    <p style={{ color: "#525252" }}>{item.username} : </p>
+
+                    <p>{item.message}</p>
+                  </div>
+                ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <h4>Your are not signed in</h4> <br />
+            <button onClick={() => signIn("github")}>
+              <span>
+                <AiOutlineGithub size={20} />
+              </span>{" "}
+              Sign in with Github
+            </button>
+            <div>
+              {loading && (
+                <ContentLoader
+                  speed={2}
+                  width="100%"
+                  height={160}
+                  viewBox="0 0 1024 160"
+                  backgroundColor="#f3f3f3"
+                  foregroundColor="#ecebeb"
+                >
+                  <rect x="-1" y="48" rx="3" ry="3" width="100%" height="40" />
+                  <rect x="-1" y="48" rx="3" ry="3" width="100%" height="40" />
+                  <rect x="-1" y="48" rx="3" ry="3" width="100%" height="40" />
+                </ContentLoader>
+              )}
+              {message &&
+                message.map((item: any, index: number) => (
+                  <div key={index}>
+                    <p style={{ color: "#525252" }}>{item.username} : </p>
+
+                    <p>{item.message}</p>
+                  </div>
+                ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default GuestBook;
+export default Guestbook;
