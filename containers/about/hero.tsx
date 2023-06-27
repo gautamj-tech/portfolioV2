73aff69918a78e456bc3dfa62b8129a47d3a7ef3
bawantha <bawantha@orelit.com>
2023-06-27 20:50:32 +0530

    fixed responsive issue

--- containers/about/hero.tsx
+++ containers/about/hero.tsx
@@ -12,7 +12,7 @@ const AboutHero = () => {
         transition={{ delay: 0.2 }}
         initial={{ opacity: 0, x: -20 }}
         animate={{ opacity: 1, x: 0 }}
-        className="title bg-secondary p-5 rounded-xl relative dark:bg-darkBg col-span-3 flex flex-col gap-5"
+        className="title bg-secondary p-5 rounded-xl relative dark:bg-darkBg col-span-3 flex flex-col gap-5 order-1"
       >
         <motion.div
           transition={{ duration: 1.5, delay: 0.4 }}
@@ -46,7 +46,7 @@ const AboutHero = () => {
         transition={{ delay: 0.3 }}
         initial={{ opacity: 0, x: 20 }}
         animate={{ opacity: 1, x: 0 }}
-        className="img w-full h-full relative col-span-2 rounded-xl"
+        className="img w-full md:w-full h-[300px] md:h-full relative col-span-3 md:col-span-2 rounded-xl"
       >
         <Image
           src={myImg}
