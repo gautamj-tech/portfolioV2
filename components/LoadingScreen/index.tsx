3b56ed18fc80adbd2e1c9abd1d66e62657ac0975
bawantha <bawantha@orelit.com>
2023-07-07 23:03:59 +0530

    add dplash screen

--- components/LoadingScreen/index.tsx
+++ components/LoadingScreen/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingScreen = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-white">
       <div className="logo">
         <svg
           className="mySvg"
