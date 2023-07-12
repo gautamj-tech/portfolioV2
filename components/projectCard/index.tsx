dd95df4e859242b4bda22c7a34feb901bde95dcb
bawantha <bawantha@orelit.com>
2023-07-12 14:01:45 +0530

    add some gradients

--- components/projectCard/index.tsx
+++ components/projectCard/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
-import { GridPattern } from "../GridPattern";
 import { BsArrowUpRightCircle } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 
@@ -22,15 +21,7 @@ function Effect({ mouseX, mouseY, ...gridProps }: any) {
 
   return (
     <div className="pointer-events-none">
-      <div className="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50">
-        <GridPattern
-          width={72}
-          height={56}
-          x="50%"
-          className="absolute inset-x-0 inset-y-[-30%] h-[160%] w-full skew-y-[-18deg] fill-black/[0.02] stroke-black/5 dark:fill-white/1 dark:stroke-white/2.5"
-          {...gridProps}
-        />
-      </div>
+      <div className="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50"></div>
       <motion.div
         className="absolute inset-0 rounded-2xl bg-gradient-to-r from-[#D7EDEA] to-[#F4FBDF] opacity-0 transition duration-300 group-hover:opacity-100 dark:from-[#202D2E] dark:to-[#303428]"
         style={style}
@@ -38,15 +29,7 @@ function Effect({ mouseX, mouseY, ...gridProps }: any) {
       <motion.div
         className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay transition duration-300 group-hover:opacity-100"
         style={style}
-      >
-        <GridPattern
-          width={72}
-          height={56}
-          x="50%"
-          className="absolute inset-x-0 inset-y-[-30%] h-[160%] w-full skew-y-[-18deg] fill-black/50 stroke-black/70 dark:fill-white/2.5 dark:stroke-white/10"
-          {...gridProps}
-        />
-      </motion.div>
+      ></motion.div>
     </div>
   );
 }
