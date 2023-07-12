74f41c4b97f2fa8d6c321a4c6a69a844cc6e300a
bawantha <bawantha@orelit.com>
2023-07-12 23:58:55 +0530

    add meta tags

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -12,8 +12,43 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="container">
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>Bawantha Thilan - Software Engineer</title>
-        <meta name="title" content="Bawantha Thilan - Software Engineer"></meta>
+
+        <title>Bawantha Thilan — Software Engineer</title>
+        <meta name="title" content="Bawantha Thilan — Software Engineer" />
+        <meta
+          name="description"
+          content="Software Engineer , Tech Blogger and Traveller, opensource enthusiast, practicing minimalist, and Pop Music in search of flow. Working with my hands to make magic happen on the internet."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="bawantha.me" />
+        <meta
+          property="og:title"
+          content="Bawantha Thilan — Software Engineer"
+        />
+        <meta
+          property="og:description"
+          content="Software Engineer , Tech Blogger and Traveller, opensource enthusiast, practicing minimalist, and Pop Music in search of flow. Working with my hands to make magic happen on the internet."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/sliit123/image/upload/v1689186241/og_ewdw57.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="bawantha.me" />
+        <meta
+          property="twitter:title"
+          content="Bawantha Thilan — Software Engineer"
+        />
+        <meta
+          property="twitter:description"
+          content="Software Engineer , Tech Blogger and Traveller, opensource enthusiast, practicing minimalist, and Pop Music in search of flow. Working with my hands to make magic happen on the internet."
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/sliit123/image/upload/v1689186241/og_ewdw57.png"
+        />
       </Head>
       <Navbar />
       {children}
