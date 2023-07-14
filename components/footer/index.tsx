4f352f5c6cce2c50d61861801283c8ee0b831fd5
bawantha <bawantha@orelit.com>
2023-07-14 23:11:19 +0530

    fixed footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -8,14 +8,18 @@ const Footer = () => {
         <h3 className="font-md text-textPrimary font-medium">
           &copy; {new Date().getFullYear()} Bawantha Thilan
         </h3>
-
-        <div className="social-links  gap-5 hidden sm:hidden md:flex">
-          <Link href="https://twitter.com/ThilanBawantha" target="_blank">
-            <AiOutlineTwitter className="text-textPrimary text-2xl" />
-          </Link>
-          <Link href="https://github.com/Bawanthathilan" target="_blank">
-            <AiOutlineGithub className="text-textPrimary text-2xl" />
-          </Link>
+        <div className=" flex flex-row gap-5 items-center ">
+          <span className=" hidden md:block text-textPrimary font-normal text-sm border rounded-full border-textPrimary px-5 py-1 border-opacity-10">
+            Built with NextJS , TailwindCSS and 💗
+          </span>
+          <div className="social-links  gap-5 flex">
+            <Link href="https://twitter.com/ThilanBawantha" target="_blank">
+              <AiOutlineTwitter className="text-textPrimary text-2xl" />
+            </Link>
+            <Link href="https://github.com/Bawanthathilan" target="_blank">
+              <AiOutlineGithub className="text-textPrimary text-2xl" />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
