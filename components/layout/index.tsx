2b771dcfab1a62f20ff3551c264af820331216cc
bawantha <bawantha@orelit.com>
2023-07-15 10:05:40 +0530

    add bg

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "../navbar";
 import Footer from "../footer";
 import Head from "next/head";
+import { useTheme } from "next-themes";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <div className="container">
       <Head>
@@ -51,6 +54,15 @@ const Layout = ({ children }: LayoutProps) => {
         />
       </Head>
       <Navbar />
+      {currentTheme === "dark" ? (
+        <div className="h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+        </div>
+      ) : (
+        <div className="h-full w-full bg-white">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        </div>
+      )}
       {children}
       <Footer />
     </div>
