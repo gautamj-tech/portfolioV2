4b514cbd890adf057d48b38644d7741e56241194
bawantha <bawantha@orelit.com>
2023-07-15 11:25:43 +0530

    add tilt effect

--- components/projectCard/index.tsx
+++ components/projectCard/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, MouseEvent, useCallback } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
@@ -13,6 +13,21 @@ interface ProjectCardProps {
   github: string;
   category?: string;
   tech?: string[];
+}
+
+function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let lastCall = 0;
+  return (...args: Parameters<T>) => {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return func(...args);
+  };
 }
 
 function Effect({ mouseX, mouseY, ...gridProps }: any) {
@@ -62,6 +77,28 @@ const ProjectCard = ({
     },
   };
 
+  const [rotate, setRotate] = useState({ x: 0, y: 0 });
+
+  const onMouseMovee = useCallback(
+    throttle((e: MouseEvent<HTMLDivElement>) => {
+      const card = e.currentTarget;
+      const box = card.getBoundingClientRect();
+      const x = e.clientX - box.left;
+      const y = e.clientY - box.top;
+      const centerX = box.width / 2;
+      const centerY = box.height / 2;
+      const rotateX = (y - centerY) / 4;
+      const rotateY = (centerX - x) / 4;
+
+      setRotate({ x: rotateX, y: rotateY });
+    }, 100),
+    []
+  );
+
+  const onMouseLeave = () => {
+    setRotate({ x: 0, y: 0 });
+  };
+
   return (
     <div
       onMouseMove={onMouseMove}
@@ -94,7 +131,15 @@ const ProjectCard = ({
         </div>
       </div>
 
-      <div className="image relative h-[400px]">
+      <div
+        className="image relative h-[400px] transition-[all_400ms_cubic-bezier(0.03,0.98,0.52,0.99)_0s] will-change-transform"
+        onMouseMove={onMouseMovee}
+        onMouseLeave={onMouseLeave}
+        style={{
+          transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+          transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+        }}
+      >
         <Image
           src={image}
           alt={`${title}`}
