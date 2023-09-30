caab237a41b9362bd1976f8da62f40016163d7f0
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-10-01 00:05:43 +0530

    add bg grid

--- components/blogCard/blogCard.tsx
+++ components/blogCard/blogCard.tsx
@@ -7,10 +7,10 @@ interface Props {
 
 const BlogCard = ({ post }: Props) => {
   return (
-    <div className="bg-secondary border border-grey-200 dark:border-[#27272a] p-5 md:p-10 rounded-3xl dark:bg-darkBg relative">
+    <div className="bg-grid-secondary overflow-hidden bg-grid-slate-100 border border-grey-200 dark:border-[#27272a]  rounded-3xl dark:bg-grid-[#1f1f21] relative">
       <span className="absolute w-[40%] bottom-0 right-px h-px bg-gradient-to-r from-blue-500/0 via-blue-500/40 to-blue-500/0 dark:from-blue-400/0 dark:via-blue-400/40 dark:to-blue-400/0"></span>
       <span className="absolute w-px left-0 h-[40%] bg-gradient-to-b from-blue-500/0 via-blue-500/40 to-blue-500/0 dark:from-blue-400/0 dark:via-blue-400/40 dark:to-blue-400/0"></span>
-      <div className="blog_content grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-5 gap-5">
+      <div className="blog_content p-5 md:p-10 bg-gradient-to-bl from-transparent via-white to-white dark:via-darkBg dark:to-darkBg grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-5 gap-5">
         <div className="blog_image relative col-span-5 sm:col-span-5 md:col-span-5 lg:col-span-2 h-[400px] sm:h-[400px] md:h-[400px] lg:h-auto">
           <Image
             src={post.coverImage}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
 const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+const svgToDataUri = require("mini-svg-data-uri");
 
 module.exports = withMT({
   content: [
@@ -60,5 +64,18 @@ module.exports = withMT({
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 });
