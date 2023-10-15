46040c0816045d274be7eabec02baeefec826972
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-10-15 12:28:43 +0530

    fixed error

--- config/keys.tsx
+++ config/keys.tsx
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export const SUPABASE_URL: any = process.env.NEXT_PUBLIC_SUPABASE_URL;
+export const SUPABASE_ANON_KEY: any = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET;
 export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 export const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET;
