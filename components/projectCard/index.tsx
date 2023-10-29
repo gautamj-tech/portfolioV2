29fa0031fce8b72bcaf2defed82dd580f2eef896
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-10-29 11:49:56 +0530

    remove tilt effect from project card

--- components/projectCard/index.tsx
+++ components/projectCard/index.tsx
@@ -77,28 +77,6 @@ const ProjectCard = ({
     },
   };
 
-  const [rotate, setRotate] = useState({ x: 0, y: 0 });
-
-  const onMouseMovee = useCallback(
-    throttle((e: MouseEvent<HTMLDivElement>) => {
-      const card = e.currentTarget;
-      const box = card.getBoundingClientRect();
-      const x = e.clientX - box.left;
-      const y = e.clientY - box.top;
-      const centerX = box.width / 2;
-      const centerY = box.height / 2;
-      const rotateX = (y - centerY) / 4;
-      const rotateY = (centerX - x) / 4;
-
-      setRotate({ x: rotateX, y: rotateY });
-    }, 100),
-    []
-  );
-
-  const onMouseLeave = () => {
-    setRotate({ x: 0, y: 0 });
-  };
-
   return (
     <div
       onMouseMove={onMouseMove}
@@ -133,12 +111,6 @@ const ProjectCard = ({
 
       <div
         className="image relative h-[400px] transition-[all_400ms_cubic-bezier(0.03,0.98,0.52,0.99)_0s] will-change-transform"
-        onMouseMove={onMouseMovee}
-        onMouseLeave={onMouseLeave}
-        style={{
-          transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
-          transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
-        }}
       >
         <Image
           src={image}
